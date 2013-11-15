f1ef5a9743cfbc3d125186a24a76d780df80748e
genval <gisse.encalada@gmail.com>
2013-11-15 10:18:52 -0300

    controller home vacio

---
+++ app/Controller/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+App::uses('AppController', 'Controller');
+/**
+ * Home Controller
+ *
+ * @property Home $Home
+ * @property PaginatorComponent $Paginator
+ */
+class HomeController extends AppController {
+
+/**
+ * Components
+ *
+ * @var array
+ */
+	public $components = array('Paginator');
+
+}
