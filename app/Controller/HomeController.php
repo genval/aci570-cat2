7233e73cf2fe2019adc4d1b593fc1467d1aced76
Francisco <fmunoz@live.cl>
2013-11-15 11:23:09 -0300

    home register

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -9,6 +9,24 @@ App::uses('AppController', 'Controller');
  */
 class HomeController extends AppController {
 
+  var $name = 'Users';
+  var $helpers = array('Html', 'Form');
+  var $components = array('Auth');
+  
+  function beforeFilter() {
+      $this->Auth->allow('register');
+  }
+      
+  function register() {
+      if (!empty($this->data)) {
+          if ($this->data['User']['password'] == $this->Auth->password($this->data['User']['password_confirm'])) {
+              $this->User->create();
+              $this->User->save($this->data);
+              $this->redirect(array('action' => 'index'));
+          }
+      }
+  }
+
 
 /**
  * Components
